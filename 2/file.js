6750187a8bd304b77b7af2b1108aecac558e2a6b
vijaihariharan <85122571+vijaihariharan@users.noreply.github.com>
2021-05-31 20:53:16 +0530

    Add files via upload

---
+++ 2/file.js
@@ -0,0 +1,13 @@
+function clickdp() 
+{
+ var topnav = document.getElementById("topnav"); 
+ if(topnav.className === "navitems"){
+     topnav.className += " apend";
+ }
+else
+{
+    topnav.className ="navitems"
+}
+    
+}
+
